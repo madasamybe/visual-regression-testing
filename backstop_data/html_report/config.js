13015c826efd80b71661439fa12ea69ad3cad117
Madasamy Ponraj <madasamybe@gmail.com>
2022-01-23 19:09:35 +0530

    reference updated

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20220123-125952/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20220123-190403/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "label": "BackstopJS Homepage",
@@ -20,18 +20,15 @@ report({
             "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 0.9954427083333334,
-          "misMatchPercentage": "1.00",
-          "analysisTime": 20
-        },
-        "diffImage": "../bitmaps_test/20220123-125952/failed_diff_backstop_default_BackstopJS_Homepage_0_document_0_phone.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20220123-125952/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20220123-190403/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "label": "BackstopJS Homepage",
@@ -47,13 +44,10 @@ report({
             "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 1.1662801106770835,
-          "misMatchPercentage": "1.17",
-          "analysisTime": 37
-        },
-        "diffImage": "../bitmaps_test/20220123-125952/failed_diff_backstop_default_BackstopJS_Homepage_0_document_1_tablet.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     }
   ],
   "id": "backstop_default"
--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <div className="login-form">
-       <h1>Backstop visual regression testing... ME</h1>
+       <h1>Backstop visual regression testing...</h1>
+       <p>By Madasamy</p>
       </div>
     </div>
   );
