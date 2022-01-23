9a740cee1599edcf6c0e139301f48c9f7857cc42
Madasamy Ponraj <madasamybe@gmail.com>
2022-01-23 13:00:26 +0530

    script changed

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20220123-124920/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20220123-125952/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "label": "BackstopJS Homepage",
@@ -20,18 +20,18 @@ report({
             "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 0.673828125,
-          "misMatchPercentage": "0.67",
-          "analysisTime": 23
+          "rawMisMatchPercentage": 0.9954427083333334,
+          "misMatchPercentage": "1.00",
+          "analysisTime": 20
         },
-        "diffImage": "../bitmaps_test/20220123-124920/failed_diff_backstop_default_BackstopJS_Homepage_0_document_0_phone.png"
+        "diffImage": "../bitmaps_test/20220123-125952/failed_diff_backstop_default_BackstopJS_Homepage_0_document_0_phone.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20220123-124920/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20220123-125952/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "label": "BackstopJS Homepage",
@@ -47,11 +47,11 @@ report({
             "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 1.055908203125,
-          "misMatchPercentage": "1.06",
-          "analysisTime": 46
+          "rawMisMatchPercentage": 1.1662801106770835,
+          "misMatchPercentage": "1.17",
+          "analysisTime": 37
         },
-        "diffImage": "../bitmaps_test/20220123-124920/failed_diff_backstop_default_BackstopJS_Homepage_0_document_1_tablet.png"
+        "diffImage": "../bitmaps_test/20220123-125952/failed_diff_backstop_default_BackstopJS_Homepage_0_document_1_tablet.png"
       },
       "status": "fail"
     }
